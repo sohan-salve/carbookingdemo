46b4b944ae854da4f1296bd09db22f33ecc33f2b
sohansalve <sohan@webkorps.com>
2022-08-19 15:47:14 +0530

    some-modification

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -25,22 +25,22 @@
 #   end
 # end
 class ChargesController < ApplicationController
-  rescue_from Stripe::CardError, with: :catch_exception
-  def new
-  end
+  # rescue_from Stripe::CardError, with: :catch_exception
+  # def new
+  # end
 
-  def create
-    StripeChargesServices.new(charges_params, current_user).call
-    redirect_to new_charge_path
-  end
+  # def create
+  #   StripeChargesServices.new(charges_params, current_user).call
+  #   redirect_to new_charge_path
+  # end
 
-  private
+  # private
 
-  def charges_params
-    params.permit(:stripeEmail, :stripeToken, :order_id)
-  end
+  # def charges_params
+  #   params.permit(:stripeEmail, :stripeToken, :order_id)
+  # end
 
-  def catch_exception(exception)
-    flash[:error] = exception.message
-  end
+  # def catch_exception(exception)
+  #   flash[:error] = exception.message
+  # end
 end
